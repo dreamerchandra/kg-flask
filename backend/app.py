c17296a9eff7bf6861aabba6949856343b51fd38
dreamerchandra <17010408+dreamerchandra@users.noreply.github.com>
2024-05-17 04:05:09 +0530

    feat: updated products to db

--- backend/app.py
+++ backend/app.py
@@ -1,32 +1,43 @@
 from flask import Flask, request
 from flask_cors import CORS
 
-from product import Product
+from product import Product, initialize_db
 
 app = Flask(__name__)  # creating the Flask class object
 
 
 CORS(app, resources={r"*": {"origins": "*"}})
 
-products = []
-
 @app.route("/products")  # decorator drfines the
 def home():
-    global products
+    products = Product.select()
     serialized_products = []
     for product in products:
-        serialized_products.append(product.__dict__)
+        serialized_products.append({
+            "id": product.id,
+            "name": product.name,
+            "price": product.price,
+            "img": product.img,
+            "description": product.description
+        })
     return {
         "products": serialized_products
     }
 
 @app.route("/products", methods=["POST"])
 def add_product():
-    global products
     product = request.json
-    products.append(Product(**product))
+    id = product.get("id")
+    name = product.get("name")
+    price = product.get("price")
+    img = product.get("img")
+    description = product.get("description")
+    print(product.get('id'))
+    print(name, price, img, description)
+    product_db = Product.create(name=name, price=price, img=img, description=description, id=id)
     return product
 
 
 if __name__ == "__main__":
+    initialize_db()
     app.run(debug=True, port=3000)
--- backend/product.py
+++ backend/product.py
@@ -1,7 +1,19 @@
-class Product:
-    def __init__(self, id, name, price, img, description):
-        self.name = name
-        self.price = price
-        self.id = id
-        self.img = img
-        self.description = description
+from peewee import *
+
+db = SqliteDatabase('product.db')
+
+class Product(Model):
+    name = TextField()
+    price = IntegerField()
+    id = IntegerField(primary_key=True)
+    img = TextField()
+    description = TextField()
+
+    class Meta:
+        database = db 
+
+def initialize_db():
+    db.connect()
+    # db.drop_tables([Product], safe = True)
+    db.create_tables([Product], safe = True)
+    db.close()
