3025347f6374abb3ec82ab307eda83341deca921
dreamerchandra <17010408+dreamerchandra@users.noreply.github.com>
2024-05-17 03:32:54 +0530

    feat: v2 done

---
+++ backend/product.py
@@ -0,0 +1,7 @@
+class Product:
+    def __init__(self, id, name, price, img, description):
+        self.name = name
+        self.price = price
+        self.id = id
+        self.img = img
+        self.description = description
